acd8c4935b7890908bf0fed5e757b1501e07df49
TarasVolianskyi <volianskyitv@gmail.com>
2017-10-12 17:08:46 +0300

    Initial commit

---
+++ app/src/main/java/com/volianskyi/taras/a121017_ecommerceforandroid/MainActivity.java
@@ -0,0 +1,15 @@
+package com.volianskyi.taras.a121017_ecommerceforandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
