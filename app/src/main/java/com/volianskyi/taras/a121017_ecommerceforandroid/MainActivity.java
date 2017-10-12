03f7d9cfb5df93128c53484ae32b4b01f4d6b147
TarasVolianskyi <volianskyitv@gmail.com>
2017-10-12 17:36:06 +0300

    Add new activity

--- app/src/main/java/com/volianskyi/taras/a121017_ecommerceforandroid/MainActivity.java
+++ app/src/main/java/com/volianskyi/taras/a121017_ecommerceforandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.volianskyi.taras.a121017_ecommerceforandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+initViews();
 
+    }
 
+    private void initViews() {
+        ListView listView = (ListView) findViewById(R.id.lvMainListMainActivity);
+        
     }
 }
