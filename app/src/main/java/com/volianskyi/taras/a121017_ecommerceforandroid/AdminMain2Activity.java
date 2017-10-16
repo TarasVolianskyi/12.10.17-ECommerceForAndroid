07c53cc4e84eb3b6755e640ce202f60e307ba6fe
TarasVolianskyi <volianskyitv@gmail.com>
2017-10-16 16:06:17 +0300

    add Admin Page

--- app/src/main/java/com/volianskyi/taras/a121017_ecommerceforandroid/AdminMain2Activity.java
+++ app/src/main/java/com/volianskyi/taras/a121017_ecommerceforandroid/AdminMain2Activity.java
@@ -1,27 +1,19 @@
 package com.volianskyi.taras.a121017_ecommerceforandroid;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class AdminMainActivity extends AppCompatActivity {
+public class AdminMain2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        initFAB();
+        setContentView(R.layout.activity_admin_main2);
         initView();
     }
 
@@ -33,21 +25,11 @@ public class AdminMainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(AdminMainActivity.this, "" + array[i], Toast.LENGTH_SHORT).show();
-            if(l==1){
+                Toast.makeText(AdminMain2Activity.this, "" + array[i], Toast.LENGTH_SHORT).show();
+                if(l==1){
 
-            }
+                }
 
-            }
-        });
-    }
-
-    private void initFAB() {
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent());//// TODO: 16.10.17 go to another page
             }
         });
     }
