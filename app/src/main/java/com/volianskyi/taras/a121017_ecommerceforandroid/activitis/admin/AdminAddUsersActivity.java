c3fe9c6374ed81f419e6238e453fdc9517c582c7
TarasVolianskyi <volianskyitv@gmail.com>
2017-10-22 23:54:39 +0300

    refactoring of code

--- app/src/main/java/com/volianskyi/taras/a121017_ecommerceforandroid/activitis/admin/AdminAddUsersActivity.java
+++ app/src/main/java/com/volianskyi/taras/a121017_ecommerceforandroid/activitis/admin/AdminAddUsersActivity.java
@@ -33,11 +33,11 @@ public class AdminAddUsersActivity extends AppCompatActivity implements View.OnC
 
     @Override
     public void onClick(View view) {
-        String nameOfNewUser = etNameOfNewUserFromAdmin.getText().toString();
-        String passOfNewUser = etPassOfNewUserFromAdmin.getText().toString();
+        String nameForNewUser = etNameOfNewUserFromAdmin.getText().toString();
+        String passForNewUser = etPassOfNewUserFromAdmin.getText().toString();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(DatabaseHelper.NAME_USERS_TABLE, nameOfNewUser);
-        contentValues.put(DatabaseHelper.PASS_USERS_TABLE, passOfNewUser);
+        contentValues.put(DatabaseHelper.NAME_USERS_TABLE, nameForNewUser);
+        contentValues.put(DatabaseHelper.PASS_USERS_TABLE, passForNewUser);
         getContentResolver().insert(DatabaseHelper.URI_USERS, contentValues);
         startActivity(new Intent(AdminAddUsersActivity.this, AdminUsersActivity.class));
     }
