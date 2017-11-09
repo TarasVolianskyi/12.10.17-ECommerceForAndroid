0492cc08d798dd7ee88eee0e325eab8a3da38f9d
TarasVolianskyi <volianskyitv@gmail.com>
2017-11-09 19:11:50 +0200

    refactoring of code update login page

--- app/src/main/java/com/volianskyi/taras/a121017_ecommerceforandroid/activitis/user_activities/MainActivity.java
+++ app/src/main/java/com/volianskyi/taras/a121017_ecommerceforandroid/activitis/user_activities/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(new Intent(MainActivity.this, AdminMainActivity.class));
                 }
 
+
             }
         });
     }
